b2904f7f9f4324399d42ea5ef08cb2ee2b6e7265
Mark Weaver <markweaver697@gmail.com>
2022-07-08 13:58:51 -0400

    more spelling errors

--- example/main.tf
+++ example/main.tf
@@ -6,11 +6,11 @@ module "atlantis" {
   atlantis_gh_user           = "your_github_user"
   atlantis_gh_token          = "your_github_token"
   atlantis_gh_webhook_secret = "a_webhook_secret"
-  atlanits_repo_whitelist    = "github.com/use_or_org/repo"
+  atlanits_repo_whitelist    = "github.com/user_or_org/repo"
   az_subscription_id         = "000000-0000-0000-0000-0000000"
   az_tenant_id               = "000000-0000-0000-0000-0000000"
   atlantis_whitelist_ips     = ["8.8.8.8/32","4.2.2.0/24"]
-  infracost_api_key          = "your_infactor_api_key"
+  infracost_api_key          = "your_infacost_api_key"
   enable_ssl                 = true
   ssl_pfx_file               = "your_cert_file.pfx"
   ssl_pfx_file_password      = "your_cert_file_password"
